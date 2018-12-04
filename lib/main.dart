111b2dd1a4aec635a722c9de00be503f8789bddc
tandai katsuaki <enegoli17@gmail.com>
2018-12-04 13:15:58 +0900

    pullするため

--- lib/main.dart
+++ lib/main.dart
@@ -23,20 +23,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List<Map> _posts = [
     {
       "user": {
         "name": "鈴木光",
-        "image": "https://images.performgroup.com/di/library/GOAL/10/dc/2017-08-31-beauty-main_xos0ywk31hxe1roo4yjttema0.jpg?t=916685574&quality=90&w=1280"
+        "image":
+            "https://images.performgroup.com/di/library/GOAL/10/dc/2017-08-31-beauty-main_xos0ywk31hxe1roo4yjttema0.jpg?t=916685574&quality=90&w=1280"
       },
-      "image": "https://tblg.k-img.com/restaurant/images/Rvw/50600/50600742.jpg",
+      "image":
+          "https://tblg.k-img.com/restaurant/images/Rvw/50600/50600742.jpg",
       "text": "阿部の笹かまの工場見学楽しかった！"
     },
     {
       "user": {
         "name": "Sayaka",
-        "image": "http://www.bijogoyomi.com/news/wp-content/uploads/2015/03/img21.jpg"
+        "image":
+            "http://www.bijogoyomi.com/news/wp-content/uploads/2015/03/img21.jpg"
       },
       "image": "https://www.dokka.jp/uimg/site3518-00.jpg",
       "text": "青葉城跡からの夜景はいつきても本当に綺麗"
@@ -50,13 +52,24 @@ class _MyHomePageState extends State<MyHomePage> {
         Row(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
-            ClipRRect(borderRadius: BorderRadius.circular(25.0),child: Image.network(post["user"]["image"], fit: BoxFit.cover,height: 50.0, width: 50.0,)),
-              Container(width: 8.0,),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(25.0),
+                child: Image.network(
+                  post["user"]["image"],
+                  fit: BoxFit.cover,
+                  height: 50.0,
+                  width: 50.0,
+                )),
+            Container(
+              width: 8.0,
+            ),
             Text(post["user"]["name"]),
           ],
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0,),
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+          ),
           child: Text(post["text"]),
         ),
         Image.network(post["image"]),
