0b029642a8c1811e8bae3cba609e6cda19d5c42d
dshukertjr <dshukertjr@gmail.com>
2018-12-05 08:52:19 +0900

    CachedNetworkImageをImageの代わりに使い始めた

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import 'post.dart';
 import 'model/post.dart';
@@ -53,39 +54,6 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
-  // {
-  //   "user": {
-  //     "name": "鈴木光",
-  //     "image":
-  //         "https://images.performgroup.com/di/library/GOAL/10/dc/2017-08-31-beauty-main_xos0ywk31hxe1roo4yjttema0.jpg?t=916685574&quality=90&w=1280"
-  //   },
-  //   "image":
-  //       "https://tblg.k-img.com/restaurant/images/Rvw/50600/50600742.jpg",
-  //   "text": "阿部の笹かまの工場見学楽しかった！",
-  //   "posted_at": DateTime(2018, 12, 2),
-  // },
-  // {
-  //   "user": {
-  //     "name": "Sayaka",
-  //     "image":
-  //         "http://www.bijogoyomi.com/news/wp-content/uploads/2015/03/img21.jpg"
-  //   },
-  //   "image": "https://www.dokka.jp/uimg/site3518-00.jpg",
-  //   "text": "青葉城跡からの夜景はいつきても本当に綺麗",
-  //   "posted_at": DateTime(2018, 12, 1),
-  // },
-  // {
-  //   "user": {
-  //     "name": "Yumi",
-  //     "image":
-  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQzVytxSQxlQtWISWuQHWy-WIJsW3064Ipo9U5YHv9sI5Bssb0kA"
-  //   },
-  //   "image":
-  //       "http://www.hokke.co.jp/wp/wp-content/uploads/2016/12/15203375_991274524350153_6547536103821712975_n.jpg",
-  //   "text": "今年の光のページェントは例年よりも始まるのが遅くて残念＞＜早く始まらないかな〜",
-  //   "posted_at": DateTime(2018, 11, 28),
-  // },
-
   Widget _postBlock(Post post) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4.0),
@@ -100,13 +68,16 @@ class _HomePageState extends State<HomePage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 ClipRRect(
-                    borderRadius: BorderRadius.circular(25.0),
-                    child: Image.network(
-                      post.userData.image,
-                      fit: BoxFit.cover,
-                      height: 50.0,
-                      width: 50.0,
-                    )),
+                  borderRadius: BorderRadius.circular(25.0),
+                  child: CachedNetworkImage(
+                    imageUrl: post.userData.image,
+                    width: 50.0,
+                    height:50.0,
+                    placeholder: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+                ),
                 Container(
                   width: 8.0,
                 ),
@@ -138,8 +109,11 @@ class _HomePageState extends State<HomePage> {
               style: Theme.of(context).textTheme.body1,
             ),
           ),
-          Image.network(
-            post.image,
+          CachedNetworkImage(
+            imageUrl: post.image,
+            placeholder: Center(
+              child: CircularProgressIndicator(),
+            ),
           ),
         ],
       ),
@@ -209,12 +183,18 @@ class _HomePageState extends State<HomePage> {
               title: Text(
                 "プロフィール",
               ),
+              onTap: () {
+                print("tap");
+              },
             ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text(
                 "設定",
               ),
+              onTap: () {
+                print("tap");
+              },
             ),
           ],
         ),
