910f7ae34e636b854ce5be99a3df89a7cd3ed6da
dshukertjr <dshukertjr@gmail.com>
2018-12-06 16:16:18 +0900

    テーマを少しいじった

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,34 @@ import 'home.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  ThemeData _buildTheme() {
+    final ThemeData base = ThemeData.light();
+    // final ThemeData base = ThemeData.dark();
+    return base.copyWith(
+      primaryColor: Colors.orange,
+      accentColor: Colors.cyan[400],
+      scaffoldBackgroundColor: Colors.white,
+      buttonTheme: ButtonThemeData(
+          buttonColor: Colors.cyan[400],
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50.0))),
+      textTheme: TextTheme(
+        body1: TextStyle(
+          fontSize: 15.0,
+          color: Colors.black,
+        ),
+        button: TextStyle(
+          fontSize: 16.0,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '仙台SNS',
-      theme: ThemeData(
-        primaryColor: Colors.orange,
-        accentColor: Colors.cyan[400],
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: TextTheme(
-          body1: TextStyle(
-            fontSize: 15.0,
-          ),
-          button: TextStyle(
-            fontSize: 16.0,
-          ),
-        ),
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.cyan[400],
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
-        ),
-      ),
+      theme: _buildTheme(),
       home: HomePage(),
     );
   }
